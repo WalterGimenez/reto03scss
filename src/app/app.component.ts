1ecad675a3dd8b05dfe8868d97b1e583aa09d1d3
WalterGimenez <wallegimenez@gmail.com>
2022-02-28 11:39:18 -0300

    se aplicó OnChanges, OnInit y OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnChanges, OnDestroy, OnInit,  SimpleChanges} from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnChanges, OnInit, OnDestroy {
+export class AppComponent{
   ciudades = ['Buenos Aires', 'Rosario', 'Misiones', 'Chaco'];
   name! : String;
   seleccion!: String;
@@ -16,18 +16,7 @@ export class AppComponent implements OnChanges, OnInit, OnDestroy {
   url = 'https://www.ecured.cu/images/thumb/5/57/Optimus_Prime.jpg/390px-Optimus_Prime.jpg';
 
 
-  ngOnChanges(changes: SimpleChanges): void {
-      console.log('Change ->', changes);
-  }
-
-  ngOnInit(): void{
-      console.log('OnInit -> ');
-  }
-
-  ngOnDestroy(): void {
-      console.log('Destroy');
-  }
-
+  
   ciudadClick(ciudad: String): void{
       console.log('Ciudad ->', ciudad);
       this.seleccion = ciudad;
--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,17 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit,  SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-button',
   template: `<button [ngStyle] = "{'background-color': color}" >{{label}}</button>`,
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent implements OnChanges, OnInit, OnDestroy  {
   @Input() color!: String;
   @Input() label!: String;
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Changes ->', changes);
+  }
 
   ngOnInit(): void {
+    console.log('OnInit -> ');
+  }
+  
+  ngOnDestroy(): void {
+    console.log('Destroy');
   }
 
 }
